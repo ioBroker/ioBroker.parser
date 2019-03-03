d61f5ef2a3673ee508c5e718ce020d8a7ccbb4cf
Ingo Fischer <github@fischer-ka.de>
2019-03-03 22:44:40 +0100

    add adapter-core

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 'use strict';
 
 // you have to require the utils module and call adapter function
-const utils    = require(__dirname + '/lib/utils'); // Get common adapter utils
+const utils = require('@iobroker/adapter-core'); // Get common adapter utils
 const adapter  = new utils.Adapter('parser');
 let request;
 let path;
